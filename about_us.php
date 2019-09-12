50538f75eb782e05962ae968463fdb361205b768
ronaldaddr2 <sschouse@gmail.com>
2019-09-13 04:48:37 +0700

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,209 @@
+<section class="business bg-grey roomy-70">
+</section>
+            <section id="action" class="business bg-grey roomy-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="maine_action">
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black text-uppercase"><strong>SELAMAT DATANG DI WEB KAMI</strong></h2>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-white text-uppercase"><strong> </strong></h2>
+                                </div>
+                            </div>
+							
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h3 class="text-black"><strong>KAMI</strong> adalah <strong>PARA PROFESIONAL</strong> yang <strong>BERPENGALAMAN</strong> dalam bidang <strong>PEMBUKUAN</strong> dan <strong>PERPAJAKAN</strong>.</h3>
+									<h3 class="text-black"><strong>Kehadiran kami</strong> untuk <strong>memberikan layanan pembukuan dan perpajakan</strong> bagi Usaha Kecil Menengah/Besar ataupun Start up 
+									yang ingin memulai usaha mereka dengan rapih <strong>sesuai prinsip Akuntansi dan Patuh terhadap Peraturan Perpajakan yang ada di Indonesia.</strong></h3>
+                                </div>
+                                <div class="action_item text-center">
+                                    <h3 class="text-black">Selain itu, Kami juga telah melayani lebih dari 50 Perusahaan di Indonesia untuk memulai mengembangkan bisnis mereka.</h3> 
+									<h3 class="text-black">Kinerja kami terbukti mampu membuat perusahaan mereka menjadi <strong>jauh lebih baik lagi</strong>.
+									Mulai dari segi <strong>Sistem Operasional Prosedur,Implementasi SOP, Implementasi sistem akuntansi, pembuatan software akuntansi dan keuangan</strong>,sampai dengan <strong>Audit Internal.</strong></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+			
+			
+			
+            <!--Call to  action section-->
+            <section id="action" class="business bg-grey roomy-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="maine_action">
+
+            <section id="action" class="business bg-grey roomy-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="maine_action">
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"><strong>Team Kami Yang Siap Membantu Anda :</strong></h2>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"><strong> </strong></h2>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"><strong> </strong></h2>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"><strong>Team Analyst, Accounting & Tax</strong></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+						
+						
+						
+                            <div class="col-md-4">
+                                <div class="action_item text-center">
+                                    <div class="item_img">
+                                        <img class="img-circle" src="<?php echo base_url() ?>assets/images/test-img2.jpg" alt="" width="35%" height="35%" />
+                                    </div>
+                                    <div class="item_text">
+                                        <h3>VIRYA</h3>
+                                        <h3>"Selalu ada Solusi disetiap Masalah"</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="action_item text-center">
+                                    <div class="item_img">
+                                        <img class="img-circle" src="<?php echo base_url() ?>assets/images/ltst-img-7.jpg" alt="" width="35%" height="35%" />
+                                    </div>
+                                    <div class="item_text">
+                                        <h3>NORMAN</h3>
+                                        <h3>"Hadapi Masalah dengan Berani"</h3>
+                                    </div>
+                                </div>
+                            </div>
+							
+                            <div class="col-md-4">
+                                <div class="action_item text-center">
+                                    <div class="item_img">
+                                        <img class="img-circle" src="<?php echo base_url() ?>assets/images/ltst-img-3.jpg" alt="" width="35%" height="35%" />
+                                    </div>
+                                    <div class="item_text">
+                                        <h3>TIARA</h3>
+                                        <h3>"Keputusan adalah awal dari Keuntungan"</h3>
+                                    </div>
+                                </div>
+                            </div>
+						
+                        </div>
+                    </div>
+                </div>
+				
+            <!--Featured Section-->
+
+				
+            </section>			
+			
+
+			
+            <!--Call to  action section-->
+            <section id="action" class="business bg-grey roomy-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="maine_action">
+
+            <section id="action" class="business bg-grey roomy-70">
+                <div class="container">
+                    <div class="row">
+                        <div class="maine_action">
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"><strong>Team Analyst & IT </strong></h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+						
+						
+						
+                            <div class="col-md-4">
+                                <div class="action_item text-center">
+                                    <div class="item_img">
+                                        <img class="img-circle" src="<?php echo base_url() ?>assets/images/ltst-img-4.jpg" alt="" width="40%" height="40%" />
+                                    </div>
+                                    <div class="item_text">
+                                        <h3>RONALD</h3>
+                                        <h3>"Mempermudah Segalanya"</h3>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="action_item text-center">
+                                    <div class="item_img">
+                                        <img class="img-circle" src="<?php echo base_url() ?>assets/images/ltst-img-5.jpg" alt="" width="35%" height="35%" />
+                                    </div>
+                                    <div class="item_text">
+                                        <h3>LIDYANA</h3>
+                                        <h3>"Inovasi selalu pemenang"</h3>
+                                    </div>
+                                </div>
+                            </div>
+							
+						
+                        </div>
+                    </div>
+                </div>
+				
+            <!--Featured Section-->
+
+				
+            </section>			
+			
+<section class="business bg-grey roomy-70">
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"></br></h2>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h2 class="text-black"></br></h2>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <h3 class="text-black">ALAMAT KANTOR KAMI  :</h3>
+									<h3 class="text-black"><strong>COSTA RIKA BLOK CK 7 NO. 6</strong></h3>
+									<h3 class="text-black"><strong>MODERNLAND - TANGERANG</strong></h3>									
+									<h3 class="text-black"><strong>PHONE : 0878 8989 0899</strong></h3>	
+									<h3 class="text-black"><strong>EMAIL : cskmg@kmg-consultant.com</strong></h3>										
+                                </div>
+                            </div>
+			
+</section>
+
+
---
+++ home.php
@@ -0,0 +1,189 @@
+            <section id="home" class="home bg-black fix">
+                <div class="overlay"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="main_features fix roomy-30">
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                        <div class="home_text ">
+										   <h2 class="text-white"><strong>LAYANAN KAMI</strong></h2>
+                                            <h3 class="text-white"></h3>
+                                        </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <div class="f_item_text">
+                                        <h2 class="text-white"></h2>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <div class="f_item_text">
+                                        <h2 class="text-white"></h2>
+                                    </div>
+                                </div>
+                            </div>
+
+							
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <a href="<?PHP echo site_url('Jasa_Pembukuan'); ?>" class="btn btn-default"><h2 class="text-white">JASA PEMBUKUAN</h2></a>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <a href="<?PHP echo site_url('Perpajakan'); ?>" class="btn btn-default"><h2 class="text-white">PERPAJAKAN</h2></a>
+                                </div>
+                            </div>
+							
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <a href="<?PHP echo site_url('Sop'); ?>" class="btn btn-default"><h2 class="text-white">SISTEM OPERASIONAL PROSEDUR</h2></a>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <a href="<?PHP echo site_url('Sistem_Akuntansi'); ?>" class="btn btn-default"><h2 class="text-white">SISTEM AKUNTANSI</h2></a>
+                                </div>
+                            </div>
+							
+                            <div class="col-md-12">
+                                <div class="action_item text-center">
+                                    <a href="<?PHP echo site_url('Internal_Audit'); ?>" class="btn btn-default"><h2 class="text-white">INTERNAL AUDIT</h2></a>
+                                </div>
+                            </div>
+
+							</div>
+						
+
+						
+
+                    </div><!--End off row-->
+                </div><!--End off container -->
+            </section> <!--End off Home Sections-->
+			
+
+
+            <!--product section-->
+            <section id="product" class="product">
+                <div class="container">
+                    <div class="main_product roomy-80">
+
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+
+                            </ol>
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <div class="port_item xs-m-top-30">
+                                                    <div class="port_img">
+                                                        <img src="<?php echo base_url() ?>assets/images/test-img2.jpg" alt="" />
+                                                        <div class="port_overlay text-center">
+                                                            <a href="<?php echo base_url() ?>assets/images/test-img2.jpg" class="popup-img">+</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="port_caption m-top-20">
+                                                        <h5></h5>
+                                                        <h6></h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="port_item xs-m-top-30">
+                                                    <div class="port_img">
+                                                        <img src="<?php echo base_url() ?>assets/images/ltst-img-7.jpg" alt="" />
+                                                        <div class="port_overlay text-center">
+                                                            <a href="<?php echo base_url() ?>assets/images/ltst-img-7.jpg" class="popup-img">+</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="port_caption m-top-20">
+                                                        <h5></h5>
+                                                        <h6></h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="port_item xs-m-top-30">
+                                                    <div class="port_img">
+                                                        <img src="<?php echo base_url() ?>assets/images/ltst-img-3.jpg" alt="" />
+                                                        <div class="port_overlay text-center">
+                                                            <a href="<?php echo base_url() ?>assets/images/ltst-img-3.jpg" class="popup-img">+</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="port_caption m-top-20">
+                                                        <h5></h5>
+                                                        <h6></h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-3">
+                                                <div class="port_item xs-m-top-30">
+                                                    <div class="port_img">
+                                                        <img src="<?php echo base_url() ?>assets/images/ltst-img-4.jpg" alt="" />
+                                                        <div class="port_overlay text-center">
+                                                            <a href="<?php echo base_url() ?>assets/images/ltst-img-4.jpg" class="popup-img">+</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="port_caption m-top-20">
+                                                        <h5></h5>
+                                                        <h6></h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+											
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="item">
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-sm-3">
+                                                <div class="port_item xs-m-top-30">
+                                                    <div class="port_img">
+                                                        <img src="<?php echo base_url() ?>assets/images/ltst-img-5.jpg" alt="" />
+                                                        <div class="port_overlay text-center">
+                                                            <a href="<?php echo base_url() ?>assets/images/ltst-img-5.jpg" class="popup-img">+</a>
+                                                        </div>
+                                                    </div>
+                                                    <div class="port_caption m-top-20">
+                                                        <h5></h5>
+                                                        <h6></h6>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                            <!-- Controls -->
+                            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                                <i class="fa fa-angle-left" aria-hidden="true"></i>
+                                <span class="sr-only">Previous</span>
+                            </a>
+
+                            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div><!-- End off row -->
+                </div><!-- End off container -->
+            </section><!-- End off Product section -->
+			
